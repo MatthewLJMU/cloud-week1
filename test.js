fc669bd41503d7e44db59b90c1332830028d6fc9
MatthewLJMU <m.eccleston@2018.ljmu.ac.uk>
2022-01-17 11:06:26 +0000

    ryan easteregg

--- test.js
+++ test.js
@@ -5,6 +5,10 @@ const port = 3000
 app.get('/', (req, res) => {
  res.send('Hello Virtual Machine!')
 })
+
+app.get('/Bollocks', (req, res) => {
+    res.send('I HATE RYAN THORN')
+})
 app.listen(port, () => {
  console.log(`Express Application listening at port 3000`)
 })
